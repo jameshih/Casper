a2986a7ed7366e57f484e77d351ef4981e20e9aa
jameshih <9959168+jameshih@users.noreply.github.com>
2024-01-16 15:30:42 +0800

    Added connectWallet

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,22 @@
+"use client";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { ConnectKitButton } from "connectkit";
+import { useAccount } from "wagmi";
 
 export default function Home() {
+  const { address, isConnecting, isDisconnected } = useAccount();
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-2xl h-screen flex flex-col justify-center items-center">
+        <div className="z-10 absolute right-0 top-0 p-4">
+          {address && <ConnectKitButton />}
+        </div>
         <div className="w-full h-2/3 p-12 flex flex-col items-center space-y-4">
           <Avatar className="w-24 h-24 ">
             <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
+            <AvatarFallback>JD</AvatarFallback>
           </Avatar>
           <h1 className="text-3xl font-bold uppercase">John Doe</h1>
           <p className="w-full break-all">
@@ -18,17 +25,23 @@ export default function Home() {
           </p>
         </div>
         <div className="w-full p-12 flex flex-col items-center space-y-1">
-          <Input
-            className="w-full"
-            type="number"
-            placeholder="Enter amount of GHO to tip"
-          />
-          <Button
-            className="w-full bg-black text-white hover:bg-gray-400 hover:text-white"
-            variant="outline"
-          >
-            Send Tip
-          </Button>
+          {address ? (
+            <>
+              <Input
+                className="w-full"
+                type="number"
+                placeholder="Enter amount of GHO to tip"
+              />
+              <Button
+                className="w-full bg-black text-white hover:bg-gray-400 hover:text-white"
+                variant="outline"
+              >
+                Send Tip
+              </Button>
+            </>
+          ) : (
+            <ConnectKitButton />
+          )}
         </div>
       </div>
     </div>
